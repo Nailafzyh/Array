e368f6951e554f5700b8a875693c02bfad4b5656
naila <nailabinti1@gmil.com>
2025-03-24 17:17:35 +0700

    percobaan2 Array new

--- src/modul1056array/modul2Arrayy.java
+++ src/modul1056array/modul2Arrayy.java
@@ -12,8 +12,8 @@ public class modul2Arrayy {
     public static void main (String[] args){
         //percobaan 2
         String data[][]= {{"NAMA\t","ALAMAT\t","NO.TELPON\t"},{"ABDUL\t","Kediri\t",
-            "085646668991"}, {"KUSNO\t","Trenggalek\t","085646668992"},{"PONIRAN\t",
-                "Bojonegoro\t","085646668993"}};
+            "085646668991"}, {"KUSNO\t","Trenggalek","085646668992"},{"PONIRAN\t",
+                "Bojonegoro","085646668993"}};
         
         for (int f=0; f<data.length; f++){
             for (int g=0; g<data[f].length; g++){
